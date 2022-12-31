7a35391f7acbb69e20ed89fa9378e5b981b1c161
Israeltheminer <israeliyanda5@gmail.com>
2022-12-31 19:08:35 +0100

    fix(config&package): added ui & validation packages & fixes to server rendering methods using babel

--- next.config.js
+++ next.config.js
@@ -1,21 +1,24 @@
 /* eslint-disable import/no-extraneous-dependencies */
-const withBundleAnalyzer = require('@next/bundle-analyzer')({
-  enabled: process.env.ANALYZE === 'true',
-});
+const withBundleAnalyzer = require("@next/bundle-analyzer")({
+	enabled: process.env.ANALYZE === "true"
+})
 
 module.exports = withBundleAnalyzer({
-  eslint: {
-    dirs: ['.'],
-  },
-  poweredByHeader: false,
-  trailingSlash: true,
-  basePath: '',
-  // The starter code load resources from `public` folder with `router.basePath` in React components.
-  // So, the source code is "basePath-ready".
-  // You can remove `basePath` if you don't need it.
-  reactStrictMode: true,
-  env: {
-    NEXT_SWELL_PUBLIC_STORE: process.env.NEXT_SWELL_PUBLIC_STORE,
-    NEXT_SWELL_PUBLIC_API_TOKEN: process.env.NEXT_SWELL_PUBLIC_API_TOKEN,
-  },
-});
+	eslint: {
+		dirs: ["."]
+	},
+	poweredByHeader: false,
+	trailingSlash: true,
+	// The starter code load resources from `public` folder with `router.basePath` in React components.
+	// So, the source code is "basePath-ready".
+	// You can remove `basePath` if you don't need it.
+	reactStrictMode: true,
+	env: {
+		NEXT_PUBLIC_SWELL_STORE: process.env.NEXT_PUBLIC_SWELL_STORE,
+		NEXT_PUBLIC_SWELL_API_TOKEN: process.env.NEXT_PUBLIC_SWELL_API_TOKEN
+	},
+	compiler: {
+		// ssr and displayName are configured by default
+		styledComponents: true
+	}
+})

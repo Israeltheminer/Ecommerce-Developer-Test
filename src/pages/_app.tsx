0702ab9b7ada8d8d8cd4be057b9dded163a5a585
Israeltheminer <israeliyanda5@gmail.com>
2023-01-03 20:30:50 +0100

    fix(app): type fixes

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,21 @@
 import '../styles/global.css'
+import { useEffect } from "react"
 import { Provider } from "react-redux"
+import { Store } from 'redux'
 import { store } from "@/store/store"
 import type { AppProps } from 'next/app'
 
-const MyApp = ({ Component, pageProps }: AppProps) => (
-  <Provider store={ store }>
-    <Component { ...pageProps } />
-  </Provider>
-)
+const MyApp = ({ Component, pageProps }: AppProps) => {
+  useEffect(() => {
+    if (window?.Cypress) {
+      window.store = store as Store
+    }
+  }, [])
+  return (
+    <Provider store={ store }>
+      <Component { ...pageProps } />
+    </Provider>
+  )
+}
 
 export default MyApp
---
+++ window.d.ts
@@ -0,0 +1,4 @@
+declare interface Window {
+	Cypress?: any
+	store?: any
+}
